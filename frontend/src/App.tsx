fc419c9ab66ba28b09193b20122f504911e064e4
Melaku Azene <137656530+MelakuAzene21@users.noreply.github.com>
2025-08-27 21:08:46 +0300

    remove  unnessacry files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import type React from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Dashboard from './pages/Dashboard'
-import Profile from './pages/Profile'
 import Layout from './components/layout/Layout'
 import Transfer from './pages/Transfer'
 import History from './pages/History'
-import Settings from './pages/Settings'
 
 
 const App: React.FC = () => {
@@ -15,10 +13,8 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<Navigate to="/dashboard" replace />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/profile" element={<Profile />} />
           <Route path="/transfer" element={<Transfer />} />
           <Route path="/history" element={<History />} />
-          <Route path="/settings" element={<Settings />} />
           <Route
             path="*"
             element={
